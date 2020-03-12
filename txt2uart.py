55f29fd4e4c3eed4a63c8a1387d8ce19c2ba7ae7
Jason Li <jiasen.cs08g@nctu.edu.tw>
2020-03-12 18:54:46 +0800

    update README, add argparse

--- txt2uart.py
+++ txt2uart.py
@@ -1,8 +1,16 @@
 import time
+import argparse
 from serial import Serial
 
-with open("testfile.txt",'r') as fd:
-    with Serial('/dev/ttyUSB0', 115200) as ser:
+parser = argparse.ArgumentParser(description='NCTU.OSDI.Lab course')
+parser.add_argument('--filename', metavar='PATH', default='./testfile.txt', type=str, help='path to script')
+parser.add_argument('--device', metavar='TTY',default='/dev/ttyUSB0', type=str,  help='path to UART device')
+parser.add_argument('--baud', metavar='Hz',default=115200, type=int,  help='baud rate')
+parser.add_argument('--delay', metavar='s',default=0.3, type=float,  help='delay between scripts')
+args = parser.parse_args()
+
+with open(args.filename,'r') as fd:
+    with Serial(args.device, 115200) as ser:
         line = fd.readline()
         while(line):
             ser.write(line.encode())
