e96ffefee4c6e66394ade87a45235e4581d92a91
Willy <wzhengwei6@gmail.com>
2020-03-13 14:06:16 +0800

    Complete UART

--- lab1/main.c
+++ lab1/main.c
@@ -1,5 +1,15 @@
-void main() {
-    int i = 0;
-    int j = 0;
-    while(1);
+#include "uart.h"
+
+void main()
+{
+    // set up serial console
+    uart_init();
+    
+    // say hello
+    uart_puts("Hello World!\n");
+    
+    // echo everything back
+    while(1) {
+        uart_send(uart_getc());
+    }
 }
