2a3bdd48e2953191755633bf4639cf54757ad01d
kaiiiz <at881005@gmail.com>
2020-03-16 04:03:05 +0800

    Handle Backspace

--- src/shell.c
+++ src/shell.c
@@ -8,8 +8,10 @@ void shell_input(char* cmd) {
     uart_printf("\r# ");
 
     int idx = 0;
+    cmd[0] = '\0';
     char c;
     while ((c = uart_read()) != '\n') {
+        // Backspace
         if (c == 8 || c == 127) {
             if (idx > 0){
                 idx--;
@@ -20,7 +22,7 @@ void shell_input(char* cmd) {
             cmd[idx++] = c;
             cmd[idx] = '\0';
         }
-        uart_printf("\r# %s", cmd);
+        uart_printf("\r# %s \r\e[%dC", cmd, idx + 2);
     }
 
     uart_printf("\n");
