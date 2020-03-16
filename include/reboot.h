ddf62fa2934f3b36c51b9f5b9a3496fcf5d4e90a
dada <daniel211920@gmail.com>
2020-03-16 10:17:53 -0700

    no unsigned

--- include/reboot.h
+++ include/reboot.h
@@ -2,8 +2,8 @@
 #define _REBOOT_H_
 
 #define PM_PASSWORD (0x5a000000)
-#define PM_RSTC ((volatile unsigned int*)0x3F10001c)
-#define PM_WDOG ((volatile unsigned int*)0x3F100024)
+#define PM_RSTC ((int*)0x3F10001c)
+#define PM_WDOG ((int*)0x3F100024)
 
 void reset(int tick);
 
