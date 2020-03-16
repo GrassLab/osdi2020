d9c2f641155beaa2967ba117632d092876dc94d6
cheng-yen tsai <evan11401@gmail.com>
2020-03-16 16:17:57 +0800

    finish uart

--- lab1/main.c
+++ lab1/main.c
@@ -1,5 +1,14 @@
-void main() {
-    int i = 0;
-    int j = 0;
-    
-}
+#include "uart.h"
+
+void main(){
+    // set up serial console
+    uart_init();
+
+    // say hello
+    uart_puts("Hello World!\n");
+
+    // echo everything back
+    while(1) {
+        uart_send(uart_getc());
+    }
+}	
