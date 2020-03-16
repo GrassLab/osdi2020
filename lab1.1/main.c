93d18097005fd271cf22e7f85abbd2e501266c39
hank0438 <hank0438>
2020-03-16 16:04:24 +0800

    branch more lab1 for experiment

--- lab1.1/main.c
+++ lab1.1/main.c
@@ -25,7 +25,7 @@
 
 #include "uart.h"
 //#include "power.h"
-//#include "delays.h"
+#include "delays.h"
 
 
 #define PM_PASSWORD 0x5a000000
@@ -45,24 +45,27 @@ void cancel_reset()
   set(PM_WDOG, PM_PASSWORD | 0); // number of watchdog tick
 }
 
-void timestamp() 
+void get_time() 
 {
     unsigned long freq = get_timer_freq();
     unsigned long counter = get_timer_counter();
-        
-    uart_puts("[");
-    uart_send(uart_i2c(counter/freq));
-    uart_puts(".");
-        int remain = counter % freq;
-        int precision = 10;
-        while (precision > 0) {
-            remain *= 10;
-            uart_send(uart_i2c(remain/freq));
-            remain %= freq;
-            if (remain == 0) break;
-            precision -= 1;
-        }
-        uart_puts("]\n");
+    char timestamp[10];
+
+    timestamp[0] = '[';
+    timestamp[1] = uart_i2c(counter/freq);
+    timestamp[2] = '.';
+    int remain = counter % freq;
+    int precision = 5;
+    int i = 2; 
+    while ((precision > 0) & (i < precision)) {
+        remain *= 10;
+        timestamp[i++] = uart_i2c(remain/freq);
+        remain %= freq;
+        if (remain == 0) break;
+        precision -= 1;
+    }
+    timestamp[i++] = ']';
+    timestamp[i] = '\n';
 }
 
 void main()
@@ -78,7 +81,7 @@ void main()
     while(1) {
 	
         char tmp;
-        char user_input[10];
+        char user_input[12];
         int i = 0;
         uart_send('>');
         while (i < 10) {
@@ -87,7 +90,10 @@ void main()
             uart_send(tmp);
             user_input[i++] = tmp;
         }
+        user_input[i] = '\0';
         uart_send('\n');
+        //uart_send(uart_i2c(i));
+        //uart_puts(user_input);
         if (i == 0) {
             uart_puts("wrong input\n");
         }
@@ -96,14 +102,13 @@ void main()
             uart_puts("hello world\n");
         }
         if (uart_strcmp(user_input, reboot) == 0) {
-            timestamp();
-            reset(0);
-            timestamp();
-            //uart_puts("alloha\n");
+            get_time();
+            reset(3);
+            get_time();
         }
         
         if (uart_strcmp(user_input, time) == 0) {
-            timestamp();    
+            get_time();    
         }
         if (uart_strcmp(user_input, help) == 0) {
             uart_puts("help me\n");
--- lab1.1/uart.h
+++ lab1.1/uart.h
@@ -23,5 +23,9 @@
  *
  */
 
-void power_off();
-void reset();
+void uart_init();
+void uart_send(unsigned int c);
+char uart_getc();
+void uart_puts(char *s);
+char uart_i2c(unsigned int d);
+int uart_strcmp(const char *cs, const char *ct);
