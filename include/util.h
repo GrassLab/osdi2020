72276cdca61426b6872730f67b1bf9339c532b2a
nobodyzxc <nobodyzxc.tw@gmail.com>
2020-03-17 16:46:53 +0800

    remove some funcs from util

--- include/util.h
+++ include/util.h
@@ -4,7 +4,5 @@
 extern void delay(unsigned long);
 extern void put32(unsigned long, unsigned int);
 extern unsigned int get32(unsigned long);
-extern unsigned int get_cntfrq();
-extern unsigned int get_cntpct();
 
 #endif
