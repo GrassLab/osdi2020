68f03e6441795b9ca8bab33053dbac8b0b86adaa
dada <daniel211920@gmail.com>
2020-03-17 08:43:00 -0700

    readable code

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ CC = aarch64-linux-gnu
 CFLAGS = -Wall -O2 -ffreestanding -nostdinc -nostdlib -nostartfiles
 all: kernel8.img
 
-kernel8.img:  obj/main.o obj/uart.o obj/start.o obj/reboot.o
-	${CC}-ld -Iinclude -T src/link.ld -o kernel8.elf obj/start.o obj/main.o obj/uart.o obj/reboot.o
+kernel8.img:  obj/main.o obj/uart.o obj/start.o obj/reboot.o obj/util.o
+	${CC}-ld -Iinclude -T src/link.ld -o kernel8.elf obj/start.o obj/main.o obj/uart.o obj/reboot.o obj/util.o
 	${CC}-objcopy -O binary kernel8.elf kernel8.img
 
 obj/start.o: src/start.S
@@ -19,6 +19,9 @@ obj/uart.o: src/uart.c
 
 obj/reboot.o: src/reboot.c
 	${CC}-gcc ${CFLAGS} -c src/reboot.c -o obj/reboot.o
+	
+obj/util.o: src/util.c
+	${CC}-gcc ${CFLAGS} -c src/util.c -o obj/util.o
 
 clean:
 	rm -f kernel8.elf
--- src/main.c
+++ src/main.c
@@ -1,13 +1,10 @@
 #include "../include/uart.h"
+#include "../include/util.h"
 #include "../include/reboot.h"
 void main()
 {
     int length = 0;
-	//FLAG
-	int FLAG_hello = 0;
-	int FLAG_help = 0;
-	int FLAG_reboot = 0;
-	int FLAG_timestamp = 0;
+
 	//get command
 	int HELLO = 0;
 	int HELP = 0;
@@ -17,12 +14,13 @@ void main()
 
     // set up serial console
     uart_init();
-    
 
     uart_getc();
     // welcome
     uart_puts("\r\nWelcome to Lab1\n# ");
-    
+    int i;
+	char input[100];
+	for(i=0;i<100;i++) input[i] = '0';
     // echo everything back
     for(;;)
     {
@@ -30,123 +28,60 @@ void main()
         key_in = uart_getc();
         uart_send(key_in);
 
-		//process the command
+		//compose the command
+		if(key_in != '\n')
+		{
+			input[length] = key_in;
+			length++;
+		}
 		
-		//hello
-		if(key_in == 'h' && length == 0 && FLAG_hello == 0) FLAG_hello++;
-		else if(key_in == 'e' && length == 1 && FLAG_hello == 1) FLAG_hello++;
-		else if(key_in == 'l' && length == 2 && FLAG_hello == 2) FLAG_hello++;
-		else if(key_in == 'l' && length == 3 && FLAG_hello == 3) FLAG_hello++;
-		else if(key_in == 'o' && length == 4 && FLAG_hello == 4) HELLO = 1;
-		else if(key_in != '\n') HELLO = 0;
-
-		//help
-		if(key_in == 'h' && length == 0 && FLAG_help == 0) FLAG_help++;
-		else if(key_in == 'e' && length == 1 && FLAG_help == 1) FLAG_help++;
-		else if(key_in == 'l' && length == 2 && FLAG_help == 2) FLAG_help++;
-		else if(key_in == 'p' && length == 3 && FLAG_help == 3) HELP = 1;
-		else if(key_in != '\n') HELP = 0;
+		//deal with the command
 		
-		//reboot
-		if(key_in == 'r' && length == 0 && FLAG_reboot == 0) FLAG_reboot++;
-		else if(key_in == 'e' && length == 1 && FLAG_reboot == 1) FLAG_reboot++;
-		else if(key_in == 'b' && length == 2 && FLAG_reboot == 2) FLAG_reboot++;
-		else if(key_in == 'o' && length == 3 && FLAG_reboot == 3) FLAG_reboot++;
-		else if(key_in == 'o' && length == 4 && FLAG_reboot == 4) FLAG_reboot++;
-		else if(key_in == 't' && length == 5 && FLAG_reboot == 5) REBOOT = 1;
-		else if(key_in != '\n') REBOOT = 0;
-		
-		//timestamp
-		if(key_in == 't' && length == 0 && FLAG_timestamp == 0) FLAG_timestamp++;
-		else if(key_in == 'i' && length == 1 && FLAG_timestamp == 1) FLAG_timestamp++;
-		else if(key_in == 'm' && length == 2 && FLAG_timestamp == 2) FLAG_timestamp++;
-		else if(key_in == 'e' && length == 3 && FLAG_timestamp == 3) FLAG_timestamp++;
-		else if(key_in == 's' && length == 4 && FLAG_timestamp == 4) FLAG_timestamp++;
-		else if(key_in == 't' && length == 5 && FLAG_timestamp == 5) FLAG_timestamp++;
-		else if(key_in == 'a' && length == 6 && FLAG_timestamp == 6) FLAG_timestamp++;
-		else if(key_in == 'm' && length == 7 && FLAG_timestamp == 7) FLAG_timestamp++;
-		else if(key_in == 'p' && length == 8 && FLAG_timestamp == 8) TIMESTAMP = 1;
-		else if(key_in != '\n') TIMESTAMP = 0;
-		
-		
-        length++;
         if(key_in == '\n') 
 		{
-			char count[100];
-			char fre[100];
-			int FLAG_count = 0, FLAG_fre = 0;
+			//which command
+			HELLO = StrCmp(input, "hello", length, 5);
+			HELP = StrCmp(input, "help", length, 4);
+			REBOOT  = StrCmp(input, "reboot", length, 6);
+			TIMESTAMP = StrCmp(input, "timestamp", length, 9);
+			//command detection done
+			
+			
 			unsigned long long int time_counter, time_fre;
-			int i;
+			
 			asm volatile("mrs %0, CNTPCT_EL0" : "=r" (time_counter));
 			asm volatile("mrs %0, CNTFRQ_EL0" : "=r" (time_fre));
 			if(HELLO == 1) uart_puts("\rHello World!\n");
 			else if(HELP) uart_puts("\rhello : print Hello World!\nhelp : help\nreboot : reboot rpi3\ntimestamp : get current timestamp\n");
 			else if(REBOOT)
-			{ 
+			{
+				uart_puts(" \n\r");
 				reset(3); 
 			}
 			else if(TIMESTAMP)
 			{
+				//print the timestamp
 			    uart_puts("\r[ ");
-			    //init
-			    for(i=0;i<100;i++) count[i] = '0';
-			    for(i=0;i<100;i++) fre[i] = '0';
-			    char num;
-			    i = 99;
-			    while(time_counter > 0 && i>0)
-			    {
-				    num = (time_counter % 10) + '0';
-				    time_counter = time_counter / 10;
-				    count[i--] = num;
-
-			    }
-			    i = 99;
-			    while(time_fre > 0 && i>0)
-			    {
-				    num = (time_fre % 10) + '0';
-				    time_fre = time_fre / 10;
-				    fre[i--] = num;
-			    }
-			    for(i=0;i<100;i++)
-			    {
-				    if(FLAG_count == 0)
-				    {
-					    if(count[i] == '0') continue;
-					    else 
-					    {
-						    FLAG_count = 1;
-						    uart_send(count[i]);
-					    }
-				    }
-				    else uart_send(count[i]);
-			    }
+			    Print_Int(time_counter);
 			    uart_puts(" / ");
-			    for(i=0;i<100;i++)
-			    {
-				    if(FLAG_fre == 0)
-				    {
-					    if(fre[i] == '0') continue;
-					    else
-					    {
-						    FLAG_fre = 1;
-						    uart_send(count[i]);
-					    }
-				    }
-				    else uart_send(count[i]);
-			    }
+			    Print_Int(time_fre);
 			    uart_puts(" ]\n");
 
 			}
-			else if(length != 1)uart_puts("\rcommand not found, use help!!!\n");
+			else if(length != 0)
+			{ 
+				uart_puts("\rcommand  ");
+				for(i=0;i<length;i++) uart_send(input[i]);
+			        uart_puts("  not found, use help!!!\n");
+			}
 			
-			length = 0;
+			
 			if(REBOOT == 0) uart_puts("\r# ");
-			//FLAG
-			FLAG_hello = 0;
-			FLAG_help = 0;
-			FLAG_reboot = 0;
-			FLAG_timestamp = 0;
-			//get command
+			
+
+			//init input_command
+			for(i=0;i<100;i++) input[i] = '0';
+			length = 0;
 			HELLO = 0;
 			HELP = 0;
 			REBOOT = 0;
