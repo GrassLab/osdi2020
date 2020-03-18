80004923f89f6e12acf54da595cb5f3133466b04
kaiiiz <at881005@gmail.com>
2020-03-18 16:57:16 +0800

    Add script

---
+++ script.py
@@ -0,0 +1,20 @@
+import serial
+import sys
+import time
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+try:
+    filename = sys.argv[1]
+except(IndexError):
+    print("Need filename!")
+    exit(1)
+
+try:
+    with open(filename) as f:
+        for line in f:
+            ser.write(str.encode(line))
+            time.sleep(0.1)
+except(FileNotFoundError):
+    print("File Not Found!")
+    exit(1)
