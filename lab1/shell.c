628706779828723de420cc4bf91fa1db4af5b32f
JackGrence <green1181772@gmail.com>
2020-03-18 09:54:12 +0800

    Add timestamp command

--- lab1/shell.c
+++ lab1/shell.c
@@ -15,10 +15,15 @@ shell_interactive ()
 	{
 	  uart_puts ("help - show command list\n");
 	  uart_puts ("hello - say hello\n");
+	  uart_puts ("timestamp - get current timestamp\n");
 	}
       else if (!strcmp ("hello", buf))
 	{
 	  uart_puts ("Hello World!\n");
+	}
+      else if (!strcmp ("timestamp", buf))
+	{
+	  print_time ();
 	}
       else
 	{
@@ -26,4 +31,45 @@ shell_interactive ()
 	  uart_puts (": command not found\n");
 	}
     }
+}
+
+void
+ftoa (double f, int size, char *buf)
+{
+  double probe = 1000000;
+  int cnt = 0;
+
+  while (f / probe < 1)
+    {
+      if (probe == 1)
+        break;
+      probe /= 10;
+    }
+  while (cnt < size - 1)
+    {
+      buf[cnt++] = ((char) (f / probe) % 10) + '0';
+      if (probe == 1)
+        buf[cnt++] = '.';
+      probe /= 10;
+    }
+  buf[size - 1] = '\0';
+}
+
+void
+print_time ()
+{
+  unsigned long freq;
+  unsigned long cnt;
+  double result;
+  char buf[0x20];
+  int len = 0;
+
+  asm volatile ("mrs %0, CNTFRQ_EL0\n"
+		"mrs %1, CNTPCT_EL0\n"
+		: "=r" (freq), "=r" (cnt));
+  result = (double) cnt / (double) freq;
+  ftoa (result, 0x20, buf);
+  uart_puts ("[");
+  uart_puts (buf);
+  uart_puts ("]\n");
 }
--- lab1/shell.h
+++ lab1/shell.h
@@ -4,5 +4,7 @@
 #define CMD_SIZE 0x20
 
 void shell_interactive ();
+void print_time ();
+void ftoa (double f, int size, char *buf);
 
 #endif /* ifndef SHELL */
