bd189655ccc57eda7acadb0c0e5c980fb000cf23
rum2mojito <wead3105@sjtu.edu.cn>
2020-03-18 21:11:11 +0800

    add test script

---
+++ lab1/test/test.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=115200)
+
+try:
+    with open('./test.txt') as f:
+        line = f.readline()
+        while line:
+            ser.write(line.encode())
+            print(ser.readline().decode('utf-8'))
+            
+            print(ser.readline().decode('utf-8'))
+            ser.flushOutput()
+            line = f.readline()
+            time.sleep(0.1)
+except PermissionError as e:
+        print("ERROR!");
