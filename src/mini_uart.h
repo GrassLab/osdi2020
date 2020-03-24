3e53772adaccbc0ad32116275d1ff61140acdadc
briasp8210 <briantaipei8210@gmail.com>
2020-03-24 11:51:16 +0800

    Add volatile qualifier for register address

--- src/mini_uart.h
+++ src/mini_uart.h
@@ -3,19 +3,19 @@
 
 #include "types.h"
 
-#define GPFSEL1 ((uint32_t *)0x3f200004)
-#define GPPUD ((uint32_t *)0x3f200094)
-#define GPPUDCLK0 ((uint32_t *)0x3f200098)
+#define GPFSEL1 ((volatile uint32_t *)0x3f200004)
+#define GPPUD ((volatile uint32_t *)0x3f200094)
+#define GPPUDCLK0 ((volatile uint32_t *)0x3f200098)
 
-#define AUXENB ((uint8_t *)0x3f215004)
-#define AUX_MU_IO_REG ((uint8_t *)0x3f215040)
-#define AUX_MU_IER_REG ((uint8_t *)0x3f215044)
-#define AUX_MU_IIR_REG ((uint8_t *)0x3f215048)
-#define AUX_MU_LCR_REG ((uint8_t *)0x3f21504c)
-#define AUX_MU_MCR_REG ((uint8_t *)0x3f215050)
-#define AUX_MU_LSR_REG ((uint8_t *)0x3f215054)
-#define AUX_MU_CNTL_REG ((uint8_t *)0x3f215060)
-#define AUX_MU_BAUD ((uint16_t *)0x3f215068)
+#define AUXENB ((volatile uint8_t *)0x3f215004)
+#define AUX_MU_IO_REG ((volatile uint8_t *)0x3f215040)
+#define AUX_MU_IER_REG ((volatile uint8_t *)0x3f215044)
+#define AUX_MU_IIR_REG ((volatile uint8_t *)0x3f215048)
+#define AUX_MU_LCR_REG ((volatile uint8_t *)0x3f21504c)
+#define AUX_MU_MCR_REG ((volatile uint8_t *)0x3f215050)
+#define AUX_MU_LSR_REG ((volatile uint8_t *)0x3f215054)
+#define AUX_MU_CNTL_REG ((volatile uint8_t *)0x3f215060)
+#define AUX_MU_BAUD ((volatile uint16_t *)0x3f215068)
 
 void gpio_init(void);
 void mini_uart_init(void);
