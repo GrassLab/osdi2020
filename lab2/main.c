21d21b7b0a7f6cfea0333d7043e073d9f14ce43a
0756107_cwChang <anonymous850723@gmail.com>
2020-03-26 22:41:47 +0800

    mailbox required1. get board revision and VC Core base address

---
+++ lab2/main.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2018 bzt (bztsrc@github)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "uart.h"
+#include "mbox.h"
+
+void board_ver(){
+    // get the board's unique serial number with a mailbox call
+    mbox[0] = 8*4;                  // length of the message
+    mbox[1] = MBOX_REQUEST;         // this is a request message
+    
+    //mbox[2] = MBOX_TAG_GETSERIAL;   // get serial number command
+    mbox[2] = MBOX_BOARD_REVISION;
+    mbox[3] = 8;                    // buffer size
+    mbox[4] = 8;
+    mbox[5] = 0;                    // clear output buffer
+    mbox[6] = 0;
+
+    mbox[7] = MBOX_TAG_LAST;
+
+    // send the message to the GPU and receive answer
+    if (mbox_call(MBOX_CH_PROP)) {
+        uart_puts("My board version is: ");
+        uart_hex(mbox[5]);
+        uart_puts("\n");
+    } else {
+        uart_puts("Unable to query baord version!\n");
+    }
+}
+
+void VC_Mem_base(){
+    mbox[0] = 8*4;                  // length of the message
+    mbox[1] = MBOX_REQUEST;         // this is a request message
+
+    mbox[2] = MBOX_VC_MEM_BASE;
+    mbox[3] = 8;                    // buffer size
+    mbox[4] = 8;
+    mbox[5] = 0;                    // clear output buffer
+    mbox[6] = 0;
+
+    mbox[7] = MBOX_TAG_LAST;
+
+    // send the message to the GPU and receive answer
+    if (mbox_call(MBOX_CH_PROP)) {
+        uart_puts("My VC Memory Base is: 0x");
+        uart_hex(mbox[5]);
+        uart_puts(" size: ");
+        uart_hex(mbox[6]);
+        uart_puts("bytes \n");
+    } else {
+        uart_puts("Unable to query VC Memroy Base!\n");
+    }
+}
+
+void main()
+{
+    // set up serial console
+    uart_init();
+    
+    board_ver();
+    VC_Mem_base();
+
+    // echo everything back
+    while(1) {
+        uart_send(uart_getc());
+    }
+}
