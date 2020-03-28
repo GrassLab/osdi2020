3f56597d8bd574a9513f4ffbc6754ff13182bf87
JackGrence <green1181772@gmail.com>
2020-03-28 17:59:05 +0800

    Add memcpy and strncmp

--- lab2/string.c
+++ lab2/string.c
@@ -1,3 +1,5 @@
+#include "string.h"
+
 int
 strcmp (const char *cs, const char *ct)
 {
@@ -13,4 +15,55 @@ strcmp (const char *cs, const char *ct)
 	break;
     }
   return 0;
+}
+
+void *
+memcpy (void *dest, const void *src, size_t len)
+{
+  char *d = dest;
+  const char *s = src;
+  while (len--)
+    *d++ = *s++;
+  return dest;
+}
+
+int
+strncmp (const char *s1, const char *s2, size_t n)
+{
+  unsigned char c1 = '\0';
+  unsigned char c2 = '\0';
+  if (n >= 4)
+    {
+      size_t n4 = n >> 2;
+      do
+	{
+	  c1 = (unsigned char) *s1++;
+	  c2 = (unsigned char) *s2++;
+	  if (c1 == '\0' || c1 != c2)
+	    return c1 - c2;
+	  c1 = (unsigned char) *s1++;
+	  c2 = (unsigned char) *s2++;
+	  if (c1 == '\0' || c1 != c2)
+	    return c1 - c2;
+	  c1 = (unsigned char) *s1++;
+	  c2 = (unsigned char) *s2++;
+	  if (c1 == '\0' || c1 != c2)
+	    return c1 - c2;
+	  c1 = (unsigned char) *s1++;
+	  c2 = (unsigned char) *s2++;
+	  if (c1 == '\0' || c1 != c2)
+	    return c1 - c2;
+	}
+      while (--n4 > 0);
+      n &= 3;
+    }
+  while (n > 0)
+    {
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0' || c1 != c2)
+	return c1 - c2;
+      n--;
+    }
+  return c1 - c2;
 }
--- lab2/string.h
+++ lab2/string.h
@@ -1,6 +1,9 @@
 #ifndef _LINUX_STRING_H_
 #define _LINUX_STRING_H_
 
+typedef unsigned long size_t;
 int strcmp (const char *cs, const char *ct);
+int strncmp (const char *s1, const char *s2, size_t n);
+void *memcpy (void *dest, const void *src, size_t len);
 
 #endif /* _LINUX_STRING_H_ */
