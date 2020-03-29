e027e819a1a49da0a70fb847ff913b9151cceed9
LeslieFan <lesliefan0531@gmail.com>
2020-03-29 14:46:28 +0800

    lab2 required done

---
+++ lab2/sender.py
@@ -0,0 +1,18 @@
+import serial
+import time
+import os
+
+s = serial.Serial(
+   port='/dev/ttyUSB0',\
+   baudrate=115200
+   )
+
+kernel = "kernel8.img"
+size = os.path.getsize(kernel)
+
+s.write(size.to_bytes(4, 'little'))
+
+with open(kernel, "rb") as f:
+    write_len = s.write(f.read())
+
+print("write %d bytes" % write_len)
