4c37b385de17691d757a4bd4d4dec0d51d663683
guo <qaz813887@gmail.com>
2020-03-31 09:09:05 -0700

    add lab2 and bootloader

---
+++ lab2/str.h
@@ -0,0 +1,8 @@
+
+int my_strcmp(const char *a, const char *b, int size);
+int my_strlen(const char *str);
+void my_strset(char *str, char rep, int size);
+void my_itoa(int num, char *str, int base);
+void str_reverse(char str[], int length);
+void my_strcat(char *dst, char *src);
+long my_atoi(const char* S);
