dcea16f09a36d3d4304ffc24a5bf384e404e9aeb
H.-S. Zheng <mathan0203@gmail.com>
2020-03-31 23:43:08 +0800

    Update the tool of send image

---
+++ lab2/tools/sendImage.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+usage="$(basename "$0") [-h] [-l n] [- ]
+program to send the image to indicate device
+
+where:
+    -h  show this help message
+    -k  indicate the kernel image to upload
+    -l  set the image location (default: 524288=0x80000)
+"
+
+# default arguments
+loc=$((0x80000))
+kernel=kernel8.img
+kernel_size=$(wc -c ${kernel} | awk '{print $1}')
+dev=/dev/ttyUSB0
+
+while getopts ':hl:k:d:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    k) kernel=$OPTARG
+       kernel_size=$(wc -c ${kernel} | awk '{print $1}')
+       ;;
+    l) loc=$(($OPTARG))
+       ;;
+    d) dev=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# start sending image
+echo "start sending the kernel image
+upload $kernel w/t size $kernel_size bytes @ address $(printf '0x%x\n' $loc)
+send to device: $dev"
+
+echo -ne "$loc\r" > $dev
+echo -ne "$kernel_size\r" > $dev
+echo -ne "1\r" > $dev
+sleep 0.1
+dd status=progress if=$kernel of=$dev # start loading kernel
