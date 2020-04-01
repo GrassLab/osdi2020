aeb51351f51ae968d03d46ba8e8f9f3a252c6477
chia56028 <chia56028@gmail.com>
2020-04-01 08:57:38 +0000

    modify framebuffer

--- lab2/mailbox_framebuffer/src/lfb.c
+++ lab2/mailbox_framebuffer/src/lfb.c
@@ -39,41 +39,41 @@ void lfb_init()
     mbox[0] = 35*4;
     mbox[1] = MBOX_REQUEST;
 
-    mbox[2] = 0x48003;  //set phy wh
+    mbox[2] = 0x48003;      //set physical display width/height
     mbox[3] = 8;
     mbox[4] = 8;
     mbox[5] = 1024;         //FrameBufferInfo.width
     mbox[6] = 768;          //FrameBufferInfo.height
 
-    mbox[7] = 0x48004;  //set virt wh
+    mbox[7] = 0x48004;      //set virtual display width/height
     mbox[8] = 8;
     mbox[9] = 8;
     mbox[10] = 1024;        //FrameBufferInfo.virtual_width
     mbox[11] = 768;         //FrameBufferInfo.virtual_height
 
-    mbox[12] = 0x48009; //set virt offset
+    mbox[12] = 0x48009;     //set virtual offset
     mbox[13] = 8;
     mbox[14] = 8;
     mbox[15] = 0;           //FrameBufferInfo.x_offset
     mbox[16] = 0;           //FrameBufferInfo.y.offset
 
-    mbox[17] = 0x48005; //set depth
+    mbox[17] = 0x48005;     //set depth
     mbox[18] = 4;
     mbox[19] = 4;
     mbox[20] = 32;          //FrameBufferInfo.depth
 
-    mbox[21] = 0x48006; //set pixel order
+    mbox[21] = 0x48006;     //set pixel order
     mbox[22] = 4;
     mbox[23] = 4;
     mbox[24] = 1;           //RGB, not BGR preferably
 
-    mbox[25] = 0x40001; //get framebuffer, gets alignment on request
+    mbox[25] = 0x40001;     //get framebuffer, gets alignment on request
     mbox[26] = 8;
     mbox[27] = 8;
     mbox[28] = 4096;        //FrameBufferInfo.pointer
     mbox[29] = 0;           //FrameBufferInfo.size
 
-    mbox[30] = 0x40008; //get pitch
+    mbox[30] = 0x40008;     //get pitch
     mbox[31] = 4;
     mbox[32] = 4;
     mbox[33] = 0;           //FrameBufferInfo.pitch
@@ -94,9 +94,7 @@ void lfb_init()
     }
 }
 
-/**
- * Show a picture
- */
+/* Show a picture */
 void lfb_showpicture()
 {
     int x,y;
