d9a4d699b084c6d492cca763fcfbf626dc9e4f4c
rum2mojito <wead3105@sjtu.edu.cn>
2020-04-10 19:14:34 +0800

    lab3 init

---
+++ lab3/util.h
@@ -0,0 +1,7 @@
+int pow(int a, int b);
+void reverse(char* str, int len);
+int intToStr(int x, char str[], int d);
+void ftoa(float n, char *res, int afterpoint);
+int strcmp(char *a, char *b);
+int atoi(char *input);
+int hex2int(char *hex);
