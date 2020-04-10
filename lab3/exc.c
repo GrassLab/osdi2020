2994a9ed260e9b8902a956f77895d22e76f1e06b
0756107_cwChang <anonymous850723@gmail.com>
2020-04-10 14:10:35 +0800

    lab3 basic exception handler

---
+++ lab3/exc.c
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2018 bzt (bztsrc@github)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "uart.h"
+
+void exc_context_push(){
+    asm volatile(
+        "sub    sp, sp, 0x00F8   ;"    //0xF0 = 8 * 31 
+        "str    x0, [sp, #0]        ;" 
+        "str    x1, [sp, #-8*1]     ;" "str    x2, [sp, #-8*2]     ;" "str    x3, [sp, #-8*3]     ;" "str    x4, [sp, #-8*4]     ;" "str    x5, [sp, #-8*5]      ;"
+        "str    x6, [sp, #-8*6]     ;" "str    x7, [sp, #-8*7]     ;" "str    x8, [sp, #-8*8]     ;" "str    x9, [sp, #-8*9]     ;" "str    x10, [sp, #-8*10]    ;"
+        "str    x11, [sp, #-8*11]   ;" "str    x12, [sp, #-8*12]   ;" "str    x13, [sp, #-8*13]   ;" "str    x14, [sp, #-8*14]   ;" "str    x15, [sp, #-8*15]    ;"
+        "str    x16, [sp, #-8*16]   ;" "str    x17, [sp, #-8*17]   ;" "str    x18, [sp, #-8*18]   ;" "str    x19, [sp, #-8*19]   ;" "str    x20, [sp, #-8*20]    ;"
+        "str    x21, [sp, #-8*21]   ;" "str    x22, [sp, #-8*22]   ;" "str    x23, [sp, #-8*23]   ;" "str    x24, [sp, #-8*24]   ;" "str    x25, [sp, #-8*25]    ;"
+        "str    x26, [sp, #-8*26]   ;" "str    x27, [sp, #-8*27]   ;" "str    x28, [sp, #-8*28]   ;" "str    x29, [sp, #-8*29]   ;" "str    x30, [sp, #-8*30]    ;"
+        :::
+    );
+}
+
+void exc_context_pop(){
+    asm volatile(
+        "ldr    x0, [sp, #0]        ;" 
+        "ldr    x1, [sp, #-8*1]     ;" "ldr    x2, [sp, #-8*2]     ;" "ldr    x3, [sp, #-8*3]     ;" "ldr    x4, [sp, #-8*4]     ;" "ldr    x5, [sp, #-8*5]      ;"
+        "ldr    x6, [sp, #-8*6]     ;" "ldr    x7, [sp, #-8*7]     ;" "ldr    x8, [sp, #-8*8]     ;" "ldr    x9, [sp, #-8*9]     ;" "ldr    x10, [sp, #-8*10]    ;"
+        "ldr    x11, [sp, #-8*11]   ;" "ldr    x12, [sp, #-8*12]   ;" "ldr    x13, [sp, #-8*13]   ;" "ldr    x14, [sp, #-8*14]   ;" "ldr    x15, [sp, #-8*15]    ;"
+        "ldr    x16, [sp, #-8*16]   ;" "ldr    x17, [sp, #-8*17]   ;" "ldr    x18, [sp, #-8*18]   ;" "ldr    x19, [sp, #-8*19]   ;" "ldr    x20, [sp, #-8*20]    ;"
+        "ldr    x21, [sp, #-8*21]   ;" "ldr    x22, [sp, #-8*22]   ;" "ldr    x23, [sp, #-8*23]   ;" "ldr    x24, [sp, #-8*24]   ;" "ldr    x25, [sp, #-8*25]    ;"
+        "ldr    x26, [sp, #-8*26]   ;" "ldr    x27, [sp, #-8*27]   ;" "ldr    x28, [sp, #-8*28]   ;" "ldr    x29, [sp, #-8*29]   ;" "ldr    x30, [sp, #-8*30]    ;"
+
+        "add    sp, sp, 0x00F8  ;"
+        "eret ;"
+        :::
+    );
+}
+
+
+/**
+ * common exception handler
+ */
+void exc_handler(unsigned long type, unsigned long esr, unsigned long elr, unsigned long spsr, unsigned long far)
+{
+    exc_context_push();
+
+    uart_puts("Exception type: ");
+    // print out interruption type
+    switch(type) {
+        case 0: uart_puts("Synchronous"); break;
+        case 1: uart_puts("IRQ"); break;
+        case 2: uart_puts("FIQ"); break;
+        case 3: uart_puts("SError"); break;
+    }
+    uart_puts("\nException class (EC) 0x");
+    uart_hex(esr>>26);
+    uart_puts(" ");
+    // decode exception type (some, not all. See ARM DDI0487B_b chapter D10.2.28)
+    switch(esr>>26) {
+        case 0b000000: uart_puts("Unknown"); break;
+        case 0b000001: uart_puts("Trapped WFI/WFE"); break;
+        case 0b001110: uart_puts("Illegal execution"); break;
+        case 0b010101: uart_puts("System call"); break;
+        case 0b100000: uart_puts("Instruction abort, lower EL"); break;
+        case 0b100001: uart_puts("Instruction abort, same EL"); break;
+        case 0b100010: uart_puts("Instruction alignment fault"); break;
+        case 0b100100: uart_puts("Data abort, lower EL"); break;
+        case 0b100101: uart_puts("Data abort, same EL"); break;
+        case 0b100110: uart_puts("Stack alignment fault"); break;
+        case 0b101100: uart_puts("Floating point"); break;
+        default: uart_puts("Unknown"); break;
+    }
+    
+
+    // decode data abort cause
+    if(esr>>26==0b100100 || esr>>26==0b100101) {
+        uart_puts(", ");
+        switch((esr>>2)&0x3) {
+            case 0: uart_puts("Address size fault"); break;
+            case 1: uart_puts("Translation fault"); break;
+            case 2: uart_puts("Access flag fault"); break;
+            case 3: uart_puts("Permission fault"); break;
+        }
+        switch(esr&0x3) {
+            case 0: uart_puts(" at level 0"); break;
+            case 1: uart_puts(" at level 1"); break;
+            case 2: uart_puts(" at level 2"); break;
+            case 3: uart_puts(" at level 3"); break;
+        }
+    }
+
+    // decode Instruction Specific Syndrome (ISS)
+    uart_puts("\nInstruction Specific Syndrome (ISS) 0x");
+    uart_hex(esr&0x1FFFFFF);
+
+    uart_puts("\nException return adress 0x");
+    uart_hex(elr);
+    uart_puts("\n");
+
+    // dump registers
+    // uart_puts(":\n[ESR_EL1]  ");
+    // uart_hex(esr>>32);
+    // uart_hex(esr);
+    // uart_puts("\n[ELR_EL1] Exception return address ");
+    // uart_hex(elr>>32);
+    // uart_hex(elr);
+    // uart_puts("\n[SPSR_EL1] ");
+    // uart_hex(spsr>>32);
+    // uart_hex(spsr);
+    // uart_puts("\n[FAR_EL1] Fault address ");
+    // uart_hex(far>>32);
+    // uart_hex(far);
+    // uart_puts("\n");
+    // no return from exception for now
+    // while(1);
+
+    exc_context_pop();
+}
