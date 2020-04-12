0066118356041d00269fde901f88755d5b199c84
hchsu(Arch) <hchsu0426@cs.nctu.edu.tw>
2020-04-12 16:57:18 +0800

    Start Shell In EL0

--- lab3/start.s
+++ lab3/start.s
@@ -24,19 +24,31 @@ core_0:
   ldr x0, =EL2_vector
   msr VBAR_EL2, x0
 
+  // set rw bit in hcr_el2 because we're using 64-bit kernel
+  mrs x0, hcr_el2
+  mov x1, #0x80000000
+  orr x0, x0, x1
+  msr hcr_el2, x0
+
   // set stack pointer to __bss_end__ + 0x4000
   ldr x0, =__bss_end__
   add x0, x0, #0x4000
   mov sp, x0
+
+  // setup sp_el for el0 and el1, el2 need to be set in el3
+  add x0, x0, #0x2000
+  msr sp_el0, x0
+  add x0, x0, #0x2000
+  msr sp_el1, x0
 
   // initialize .bss section
   ldr x0, =__bss_start__
   eor x1, x1, x1 // x1 = 0
   ldr x2, =__bss_end__
 
-  // jump to main if .bss length is 0
+  // jump to el2_to_el1 if .bss length is 0
   sub x3, x2, x0
-  cbz x3, main
+  cbz x3, el2_to_el1
 
 bss_reset_loop:
   str x1, [x0], #0x8
@@ -45,9 +57,27 @@ bss_reset_loop:
 
   cbnz x3, bss_reset_loop
 
+el2_to_el1:
+  // setup spsr_el2
+  // Ref: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0488c/CHDDGJID.html
+  // Use el1h -> sp_el1 -> 0x5
+  mov x0, 0x5
+  msr spsr_el2, x0
+
+  // Use eret to el1
+  adr x0, el1_to_el0
+  msr elr_el2, x0
+  eret
+el1_to_el0:
+  eor x0, x0, x0
+  msr spsr_el1, x0
+  adr x0, main
+  msr elr_el1, x0
+  eret // return to main
+
   // Jump to main
   // main should not return or the stack will be smashed
-  bl main
+  //bl main
 
   // If main return, enter low power mode
   b other_core
