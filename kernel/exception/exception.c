5be3364739bc42fa0729a1485c163385f33cb326
Tony <jingwang.cs08g@nctu.edu.tw>
2020-04-13 14:11:45 +0800

    add function to get surrent el

--- kernel/exception/exception.c
+++ kernel/exception/exception.c
@@ -1,6 +1,8 @@
 #include "lib/type.h"
 #include "kernel/peripherals/uart.h"
 
+#include "exception.h"
+
 void exec_controller ( uint64_t elr, uint64_t esr )
 {
     int ec = (((uint32_t)esr)>>26);
@@ -12,4 +14,19 @@ void exec_controller ( uint64_t elr, uint64_t esr )
     uart_printf("Instruction specific syndrome (ISS): %x\n", iss);
 
     // asm volatile ( "eret;" );
+}
+
+int get_current_el ()
+{
+    int el;
+    asm volatile (
+        "mrs x0, CurrentEL;"
+        "mov %0, x0;" 
+        : "=r"(el)
+    );
+
+    el >>= 2;
+    el &= 2;
+
+    return el;
 }
--- kernel/exception/exception.h
+++ kernel/exception/exception.h
@@ -4,5 +4,6 @@
 #include "lib/type.h"
 
 void exec_controller ( uint64_t esr, uint64_t elr );
+int get_current_el ( );
 
 #endif
