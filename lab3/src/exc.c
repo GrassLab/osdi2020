880720154aa0618461b4e0173a72b3416540a65b
chia56028 <chia56028@gmail.com>
2020-04-14 02:50:19 +0800

    add exception.S

--- lab3/src/exc.c
+++ lab3/src/exc.c
@@ -27,12 +27,14 @@
 #include "printf.h"
 
 
-void
-exception_handler (void)
+void    exception_handler (void)
 {
   unsigned long elr_el2, esr_el2;
-  asm volatile ("mrs %0, elr_el2\n"
-		"mrs %1, esr_el2\n":"=r" (elr_el2), "=r" (esr_el2));
+  asm volatile (
+    "mrs %0, elr_el2\n"
+    "mrs %1, esr_el2\n"
+    :"=r" (elr_el2), "=r" (esr_el2)
+  );
   printf ("Exception return address 0x%p\r\n", (void *) elr_el2);
   printf ("Exception class (EC) 0x%x\r\n", esr_el2 >> 26);
   printf ("Instruction specific syndrome (ISS) 0x%x\r\n", esr_el2 & 0xffffff);
