e54b0082ea216ec2114e097b757328bf39808564
H.-S. Zheng <mathan0203@gmail.com>
2020-04-14 23:22:19 +0800

    Add uart_print decl.

--- lab3/include/libc.h
+++ lab3/include/libc.h
@@ -10,4 +10,5 @@ int sstrcmp(const char *s1, const char *s2);
 void *memset(void *s, int c, size_t n);
 void memcpy(void *dest, const void *src, size_t num);
 void uart_println(char *format, ...);
+void uart_print(char *format, ...);
 void  __attribute__((noreturn)) abort();
