cf311d508c0a50c5f10613ff0d417312787c8fbf
Abekabe <ms0229643@gmail.com>
2020-04-15 18:05:17 +0800

    Added irq

--- src/exc.c
+++ src/exc.c
@@ -1,25 +1,28 @@
 #include "uart.h"
 //#include "timer.h"
 
-void exc_function(unsigned long x0, unsigned long x1, unsigned long x2, unsigned long x3){
+void exc_function(){
 	unsigned long esr_el2, elr_el2;
   	unsigned int ec, iss;
     asm volatile ("mrs %0, ESR_EL2\n": "=r" (esr_el2));
 	asm volatile ("mrs %0, ELR_EL2\n": "=r" (elr_el2));
 	ec = esr_el2 >> 26;
   	iss = esr_el2 & 0xFFFFFF;
-	uart_puts("Exception return address ");
-	uart_hex(elr_el2);
-  	uart_puts("\n\rException class (EC) ");
-	uart_hex(ec);
- 	uart_puts("\n\rInstruction specific syndrome (ISS) ");
-	uart_hex(iss);
-	uart_puts("\n\r");
-    if (iss == 2) {
+    if (iss == 1) {
+	    uart_puts("Exception return address ");
+	    uart_hex(elr_el2);
+  	    uart_puts("\n\rException class (EC) ");
+	    uart_hex(ec);
+ 	    uart_puts("\n\rInstruction specific syndrome (ISS) ");
+	    uart_hex(iss);
+	    uart_puts("\n\r");
+    } else if (iss == 2) {
         uart_puts("enable time\n");
+        init_irq();
+        enable_irq();
         core_timer_enable();
         local_timer_enable();
-        enable_irq();
+        while(1);
     }
 }
 
--- src/timer.c
+++ src/timer.c
@@ -3,6 +3,7 @@
 #define LOCAL_TIMER_CONTROL ((volatile unsigned int *)0x40000034)
 #define LOCAL_TIMER_IRQ_CLR ((volatile unsigned int *)0x40000038)
 #define LOCAL_TIMER_IRQ_CTRL ((volatile unsigned int *)0x40000040)
+#define CORE0_IRQ_SOURCE ((volatile unsigned int *)0x40000060)
 
 int core_timer_counter = 0;
 int local_timer_counter = 0;
@@ -15,8 +16,9 @@ void core_timer_enable() {
     asm volatile("mov x0, 2");
     asm volatile("ldr x1, =0x40000040");
     asm volatile("str x0, [x1]");
-    asm volatile("mov x0, 0xffffff");
+    asm volatile("mov x0, 0xfffffff");
     asm volatile("msr cntp_tval_el0, x0");
+    uart_puts("core timer setup\n");
     while (b > 0) {
     asm volatile ("mrs %0, cntp_ctl_el0" : "=r"(a));
     asm volatile ("mrs %0, cntp_tval_el0" : "=r"(b));
@@ -36,15 +38,15 @@ void core_timer_handler() {
     uart_puts("Core timer interrupt, ");
     uart_int(core_timer_counter);
     uart_puts(" times\n");
-    asm volatile("mov x0, 0xffffff");
+    asm volatile("mov x0, 0xfffffff");
     asm volatile("msr cntp_tval_el0, x0");
 }
-
 
 void local_timer_enable(){
     unsigned int flag = 0x30000000; // enable timer and interrupt.
     unsigned int reload = 25000000;
     *LOCAL_TIMER_CONTROL = flag | reload;
+    uart_puts("local timer setup\n");
 }
 
 void local_timer_handler(){
@@ -54,4 +56,15 @@ void local_timer_handler(){
     uart_puts(" times\n");
 
     *LOCAL_TIMER_IRQ_CLR = 0xc0000000; // clear interrupt and reload.
+}
+
+void irq_router ()
+{
+  unsigned int IRQ_SOURCE = *CORE0_IRQ_SOURCE;
+  if (IRQ_SOURCE == 1 << 1)
+    core_timer_handler();
+  else if (IRQ_SOURCE == 1 << 11)
+    local_timer_handler();
+  else
+    return;
 }
