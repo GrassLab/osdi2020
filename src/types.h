8458ee76007296ea8712a6431c4b7b6dbb5c9740
briasp8210 <briantaipei8210@gmail.com>
2020-04-16 21:01:00 +0800

    Replace types definition with standard libraries

--- src/types.h
+++ src/types.h
@@ -1,19 +1,8 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-typedef char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef long long int64_t;
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-typedef enum {
-  false,
-  true
-} bool;
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #endif // TYPES_H_
