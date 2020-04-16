c7c3fc8aa5800417aa6eec78210fec9f3eb6a80d
LeslieFan <lesliefan0531@gmail.com>
2020-04-16 16:10:43 +0800

    kernel.c cleaner and safer

--- lab2/kernel.c
+++ lab2/kernel.c
@@ -5,8 +5,6 @@
 #define LOADER_SIZE_MAX 0x10000
 
 extern char __loader_size[];
-unsigned int loader_size;
-char *gpu_load_addr ;
 
 unsigned int get_kernel_size(){
     unsigned int size;
@@ -17,91 +15,38 @@ unsigned int get_kernel_size(){
     return size;
 }
 
-unsigned long calc_loader_copy_addr(unsigned long load_kernel_addr){
-    unsigned long copy_addr;
-    loader_size = (unsigned long)__loader_size;
-    // uart_dec(loader_size); uart_puts("\n");
-    // unsigned int stack_reserve = 1024;
-    copy_addr = load_kernel_addr - LOADER_SIZE_MAX;
-    // uart_dec(copy_addr); uart_puts("\n");
-    return copy_addr;
-}
-
-// use inline asm to modify x30 in function recieve_kernel and copy_loader_and_jumpor 
-// should not include any function call in them 
-// or the compiler generated code would not be as expected(due to the stack operation)
-void recieve_kernel(unsigned long load_addr, unsigned int size){
-    // uart_puts("recieve start...\n");
+void recieve_kernel(unsigned long load_addr, unsigned int size, unsigned long copy_addr){
     char *kernel=(char*)load_addr;
     
-    // important!!! the ori *uart_getc would be overwirte, too
-    char (*uart_getc_copied)() = calc_loader_copy_addr(load_addr) + (uart_getc - LOADER_START_ADDR); 
+    char (*uart_getc_copied)() = copy_addr + (uart_getc - LOADER_START_ADDR); 
     
+    uart_puts("\nloading kernel image...\n\n\n");
     while(size--) 
         *kernel++ = uart_getc_copied();
-    // uart_puts("recieve done...\n");
    
     asm volatile ("mov x30, %0; ret" ::"r"(load_addr)); 
-    //should return immediately in asm line above or the compiler will load other address to x30 then retrun
 }
 
 void copy_loader_and_jump(char *copy_addr, char *load_kernel_addr, unsigned int kernel_size){
-    gpu_load_addr = (char*)LOADER_START_ADDR;
-
     //copy loader
-    char *loader_old_base = gpu_load_addr;
+    char *loader_old_base = (char*)LOADER_START_ADDR;
     char *loader_new_base = copy_addr;
-    unsigned int loader_byte = loader_size ;
-    // uart_dec(loader_byte); uart_puts("\n");
+    unsigned int loader_byte = (unsigned long)__loader_size ;
     while(loader_byte--)
         *loader_new_base++ = *loader_old_base++;
-    // uart_hex((unsigned long)loader_old_base); uart_puts("\n");
-    // uart_hex((unsigned long)loader_new_base); uart_puts("\n");
 
     //copy stack
-    char *old_stack = gpu_load_addr-1;
+    char *old_stack = (char*)LOADER_START_ADDR-1;
     char *new_stack = copy_addr-1;
     char* sp;
     asm volatile ("mov %0, sp" :"=r"(sp));
-    // uart_dec(LOADER_START_ADDR-(unsigned long)sp); uart_puts("\n");
     while(old_stack >= sp)
         *new_stack-- = *old_stack--;
     sp = new_stack + 1;
     asm volatile ("mov sp, %0" ::"r"(sp));
     
-    // jump to copied loader 
-    // when the x30 is determined, below should not include any function call!!
-    // no problem, but contain magic number... 
-    // unsigned int next_instr_in_copy = (unsigned long)copy_addr + 0x84c; //magic number...-> offset to next instrc(objdump). 
-    // asm volatile ("mov x30, %0" ::"r"(next_instr_in_copy));
-
-
-    // when using inline asm, becareful of the register to write!!! 
-    // ex: below will cause error, due to x0 save the copy_addr value!!!
-    // then the result will be very unstable
-    /*uart_puts("loader backup done\n");
-      asm volatile ("ldr x0, =0x10000; sub x30, x30, x0");*/
-    //I made a stuuuuuupid mistake -> asm volatile ("sub x30, x30, #0x10000"); 
-    asm volatile ("sub x30, x30, #0x80000;"
-                  "add x30, x30, %0"::"r"(copy_addr)); 
-    
-    // unsigned long lr;
-    // asm volatile ("mov %0, x30" :"=r"(lr));
-    // uart_hex(lr); uart_puts("\n");
-    // uart_hex((unsigned long)copy_addr); uart_puts("\n");
-    // uart_hex( lr - (unsigned long)copy_addr); uart_puts("\n");
-
-     //note: stack operation is restricted when using inline asm
-
-    // the function pointer also needs to points to the copied function
-    /*uart_getc = (unsigned long)copy_addr + (uart_getc - LOADER_START_ADDR);
-      uart_send = (unsigned long)copy_addr + (uart_send - LOADER_START_ADDR);*/ 
-    // the function pointer its self should not be overwrite aswell!!
-    // there is no way to modified a object address (&uart_getc = &uart_getc - LOADER_SIZE_MAX)
-    // uart_hex(&uart_getc); uart_puts("\n");
-    // uart_hex(uart_getc); uart_puts("\n");
-    // uart_hex(*((volatile unsigned int*)(0x00087118))); uart_puts("\n");
-    // uart_hex(*((volatile unsigned int*)(0x00077118))); uart_puts("\n");
+    void (*recieve_kernel_copied)(unsigned long, unsigned int, unsigned long) =  (unsigned long )copy_addr + (recieve_kernel - LOADER_START_ADDR);
+    recieve_kernel_copied((unsigned long)load_kernel_addr, kernel_size, (unsigned long)copy_addr);
 }
 
 void load_kernel_img(){
@@ -118,12 +63,8 @@ void load_kernel_img(){
     uart_puts("...\n");
     unsigned long load_addr_ul;
     load_addr_ul = (*load_addr) ? atoi(load_addr) : 0x80000; 
-    // uart_hex(addr_ul);
-    // uart_puts("\n");
 
     unsigned long loader_copy_addr;
-    loader_copy_addr = calc_loader_copy_addr(load_addr_ul);
+    loader_copy_addr = load_addr_ul - LOADER_SIZE_MAX;
     copy_loader_and_jump((char*)loader_copy_addr, (char*)load_addr_ul, kernel_size);
-    uart_puts("\nloading kernel image...\n\n\n");
-    recieve_kernel(load_addr_ul, kernel_size);
 }
