d6d6ef92fff8b681bbc0327c844642180ff1cead
JackGrence <green1181772@gmail.com>
2020-04-30 18:11:28 +0800

    Check reschedule flag

--- sched/sched.c
+++ sched/sched.c
@@ -8,6 +8,9 @@ void
 schedule ()
 {
   struct task_struct *pos = current;
+  if (!pos->resched)
+    return;
+  pos->resched = 0;
   list_move_tail (&current->list, runqueue);
   switch_to (current, list_entry (runqueue->next, struct task_struct, list));
 }
