80dd0cece6dff94edf918896e23cd8c4017c5ee0
Tony <jingwang.cs08g@nctu.edu.tw>
2020-05-02 19:34:05 +0800

    make exception table prettier

--- kernel/exception/exception.c
+++ kernel/exception/exception.c
@@ -11,6 +11,11 @@
 #include "irq.h"
 #include "timer.h"
 
+#define SYS_CALL_CASE( CASE, FUNC ) \
+    case CASE:                      \
+        FUNC;                       \
+        break;
+
 void exec_controller_el1 ( int sys_call_type )
 {
     uint64_t elr, esr;
@@ -25,68 +30,34 @@ void exec_controller_el1 ( int sys_call_type )
 
     switch ( sys_call_type )
     {
-        case SYS_TEST_SVC:
-            print_exec_info ( EL1, elr, esr );
-            break;
-        case SYS_IRQ_EL1_ENABLE:
-            irq_el1_enable ( );
-            break;
-        case SYS_IRQ_EL1_DISABLE:
-            irq_el1_disable ( );
-            break;
-        case SYS_CORE_TIMER_ENABLE:
-            sys_core_timer_enable ( );
-            break;
-        case SYS_CORE_TIMER_DISABLE:
-            sys_core_timer_disable ( );
-            break;
-        case SYS_GET_TIMESTAMP:
-            argus[0] = (double) sys_get_current_timestamp ( );
-            break;
-        case SYS_SCHEDULE:
-            sys_do_schedule ( );
-            break;
-        case SYS_DO_EXEC:
-            sys_do_exec ( ( void ( * ) ( ) ) ( intptr_t ) ( argus[0] ) );
-            break;
-        case SYS_WAIT_MSEC:
-            sys_wait_msec ( (unsigned int) ( argus[0] ) );
-            break;
-        case SYS_DO_EXIT:
-            sys_do_exit ( current_thread );
-            break;
-        case SYS_GET_PID:
-            argus[0] = current_thread->task_id; /* set the return value */
-            break;
-        case SYS_UART_WRITE:
-            uart_puts ( (char *) ( intptr_t ) ( argus[0] ) );
-            break;
-        case SYS_UART_READ:
-            argus[0] = (char) ( uart_getc ( ) ); /* set the return value */
-            break;
-        case SYS_GET_VC_BASE_ADDR:
-            argus[0] = ( uint64_t ) ( mbox_get_VC_base_addr ( ) ); /* set the return value */
-            break;
-        case SYS_GET_BOARD_REVISION:
-            argus[0] = ( uint32_t ) ( mbox_get_board_revision ( ) );
-            break;
-        case SYS_LOCAL_TIMER_ENABLE:
-            sys_local_timer_enable ( );
-            break;
-        case SYS_LOCAL_TIMER_DISABLE:
-            sys_local_timer_disable ( );
-            break;
-        case SYS_FORK:
-            /* set the return value as child pid */
-            argus[0] = sys_duplicate_task ( current_thread )->task_id;
-            break;
-        case SYS_CLEAR_ZOMBIE:
-            clear_zombie ( );
-            break;
+        // clang-format off
+        SYS_CALL_CASE ( SYS_IRQ_EL1_ENABLE,         irq_el1_enable          ( ) )
+        SYS_CALL_CASE ( SYS_IRQ_EL1_DISABLE,        irq_el1_disable         ( ) )
+        SYS_CALL_CASE ( SYS_CORE_TIMER_ENABLE,      sys_core_timer_enable   ( ) )
+        SYS_CALL_CASE ( SYS_CORE_TIMER_DISABLE,     sys_core_timer_disable  ( ) )
+        SYS_CALL_CASE ( SYS_SCHEDULE,               sys_do_schedule         ( ) )
+        SYS_CALL_CASE ( SYS_LOCAL_TIMER_ENABLE,     sys_local_timer_enable  ( ) )
+        SYS_CALL_CASE ( SYS_LOCAL_TIMER_DISABLE,    sys_local_timer_disable ( ) )
+        SYS_CALL_CASE ( SYS_CLEAR_ZOMBIE,           clear_zombie            ( ) )
+
+        SYS_CALL_CASE ( SYS_TEST_SVC,               print_exec_info     ( EL1, elr, esr ) )
+        SYS_CALL_CASE ( SYS_WAIT_MSEC,              sys_wait_msec       ( (unsigned int) ( argus[0] ) ) )
+        SYS_CALL_CASE ( SYS_DO_EXEC,                sys_do_exec         ( ( void ( * ) ( ) ) ( intptr_t ) ( argus[0] ) ) )
+        SYS_CALL_CASE ( SYS_DO_EXIT,                sys_do_exit         ( current_thread ) )
+        SYS_CALL_CASE ( SYS_UART_WRITE,             uart_puts           ( (char *) ( intptr_t ) ( argus[0] ) ) )
+
+        SYS_CALL_CASE ( SYS_GET_PID,                argus[0] = current_thread->task_id )
+        SYS_CALL_CASE ( SYS_FORK,                   argus[0] = sys_duplicate_task  ( current_thread )->task_id )
+        SYS_CALL_CASE ( SYS_GET_TIMESTAMP,          argus[0] = (double) sys_get_current_timestamp ( ) )
+        SYS_CALL_CASE ( SYS_UART_READ,              argus[0] = (char) ( uart_getc ( ) ) )
+        SYS_CALL_CASE ( SYS_GET_VC_BASE_ADDR,       argus[0] = ( uint64_t ) ( mbox_get_VC_base_addr ( ) ) )
+        SYS_CALL_CASE ( SYS_GET_BOARD_REVISION,     argus[0] = ( uint32_t ) ( mbox_get_board_revision ( ) ) )
+        
         default:
             print_exec_info ( EL1, elr, esr );
             break;
-    }
+        // clang-format on      
+    } 
 }
 
 void print_exec_info ( EL el, uint64_t elr, uint64_t esr )
