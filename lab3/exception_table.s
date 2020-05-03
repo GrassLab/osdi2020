1c362f5f889c822befaf13f012c33516b50af42c
Yuwei <yuwei.shih.tw@gmail.com>
2020-05-03 11:58:58 +0800

    Lab3 (#5)
    * lab2 init
    
    * add mbox and PL011
    
    * init loadimg
    
    * send kernel done. TODO: jump to new kernel.
    
    * load new kernel done!
    
    * add hex convert
    
    * load image fail
    
    * lab3 init
    
    * add entry file
    
    * ignore *.d file
    
    * add exception basic function
    
    * exc workable
    
    * r2 context saving
    
    * add *.s file
    
    * timer interrupt
    
    * timer ok
    
    * refine strat.S
    
    * rename exception table
    
    * remove interrupt.s
    
    * el switch
    
    * syscall
    
    * refine timaer
    
    * workable
    
    * done
    
    Co-authored-by: t0037799 <asdqerdfethtte@gmail.com>
    Co-authored-by: Yuxin Zhou <yqkqknct@gmail.com>
    Co-authored-by: 李東霖 <lee097019@gmail.com>

---
+++ lab3/exception_table.s
@@ -0,0 +1,156 @@
+.macro context_switch
+sub	sp, sp, #256
+stp	x0, x1, [sp, #16 * 0]
+stp	x2, x3, [sp, #16 * 1]
+stp	x4, x5, [sp, #16 * 2]
+stp	x6, x7, [sp, #16 * 3]
+stp	x8, x9, [sp, #16 * 4]
+stp	x10, x11, [sp, #16 * 5]
+stp	x12, x13, [sp, #16 * 6]
+stp	x14, x15, [sp, #16 * 7]
+stp	x16, x17, [sp, #16 * 8]
+stp	x18, x19, [sp, #16 * 9]
+stp	x20, x21, [sp, #16 * 10]
+stp	x22, x23, [sp, #16 * 11]
+stp	x24, x25, [sp, #16 * 12]
+stp	x26, x27, [sp, #16 * 13]
+stp	x28, x29, [sp, #16 * 14]
+str	x30, [sp, #16 * 15] 
+.endm
+
+.macro	context_switch_back
+ldp	x0, x1, [sp, #16 * 0]
+ldp	x2, x3, [sp, #16 * 1]
+ldp	x4, x5, [sp, #16 * 2]
+ldp	x6, x7, [sp, #16 * 3]
+ldp	x8, x9, [sp, #16 * 4]
+ldp	x10, x11, [sp, #16 * 5]
+ldp	x12, x13, [sp, #16 * 6]
+ldp	x14, x15, [sp, #16 * 7]
+ldp	x16, x17, [sp, #16 * 8]
+ldp	x18, x19, [sp, #16 * 9]
+ldp	x20, x21, [sp, #16 * 10]
+ldp	x22, x23, [sp, #16 * 11]
+ldp	x24, x25, [sp, #16 * 12]
+ldp	x26, x27, [sp, #16 * 13]
+ldp	x28, x29, [sp, #16 * 14]
+ldr	x30, [sp, #16 * 15] 
+add	sp, sp, #256
+eret
+.endm
+
+// Simple vector table
+.global el2_exception_table
+.align 11 // vector table should be aligned to 0x800
+el2_exception_table:
+    b el_not_implement // branch to a handler function.
+    .align 7 // entry size is 0x80, .align will pad 0
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+    b el2_exception_handler
+    .align 7
+
+    context_switch
+    bl irq_handler
+    context_switch_back
+    .align 7
+
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+el2_exception_handler:
+    context_switch
+    mov     x0, #0
+    mrs     x1, esr_el2
+    mrs     x2, elr_el2
+    mrs     x3, spsr_el2
+    mrs     x4, far_el2
+    bl      _el2_exception_handler
+    context_switch_back
+
+// Simple vector table
+.global el1_exception_table
+.align 11 // vector table should be aligned to 0x800
+el1_exception_table:
+    b el_not_implement // branch to a handler function.
+    .align 7 // entry size is 0x80, .align will pad 0
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+    // from el0
+    b el1_exception_handler
+    .align 7
+    b el1_irq_handler
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+    b el_not_implement
+    .align 7
+
+el1_exception_handler:
+    context_switch
+    mov     x0, #0
+    mrs     x1, esr_el1
+    mrs     x2, elr_el1
+    mrs     x3, spsr_el1
+    mrs     x4, far_el1
+    bl      _el1_exception_handler
+    context_switch_back
+    
+el1_irq_handler:
+    context_switch
+    bl      irq_handler
+    // bl      _context_switch_msg
+    context_switch_back
+
+el_not_implement:
+    context_switch
+    bl      _not_implement
+    context_switch_back
