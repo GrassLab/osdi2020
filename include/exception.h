5b877e0a2e4c20e42ef47dab89f5fbf92ebc112a
kaiiiz <at881005@gmail.com>
2020-05-06 04:03:52 +0800

    Initialize trap_frame

--- include/exception.h
+++ include/exception.h
@@ -1,5 +1,17 @@
 #include "stdint.h"
 
+#ifndef __EXC_H__
+#define __EXC_H__
+
+struct trapframe {
+    uint64_t x[31]; // general register from x0 ~ x30
+    uint64_t sp_el0;
+    uint64_t elr_el1;
+    uint64_t spsr_el1;
+};
+
+#endif
+
 extern uint64_t arm_core_timer_jiffies, arm_local_timer_jiffies;
 extern uint64_t cntfrq_el0, cntpct_el0;
 
