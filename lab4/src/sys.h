79c6b41693cc86eec584c0e47be9aad824885649
chihcenyang <chichensmile@gmail.com>
2020-05-06 15:13:02 +0800

    lab4

---
+++ lab4/src/sys.h
@@ -0,0 +1,18 @@
+#ifndef   _SYS_H
+#define _SYS_H
+
+#define __NR_syscalls       6
+
+#define SYS_GET_TASKID_NUMBER 0 
+#define SYS_FORK_NUMBER       1   
+#define SYS_EXIT_NUMBER       2   
+#define SYS_EXEC_NUMBER       3
+#define SYS_UART_READ_NUMBER  4
+#define SYS_UART_WRITE_NUMBER 5
+
+
+#ifndef __ASSEMBLER__
+
+
+#endif
+#endif  /*_SYS_H */
