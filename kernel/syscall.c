49dbb3d7cf020252098620f1ef714e861a9c5950
briasp8210 <briantaipei8210@gmail.com>
2020-05-06 20:50:36 +0800

    Disable lock in uart_read and uart_write

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -8,17 +8,16 @@ uint32_t do_get_taskid(void) {
 }
 
 size_t do_uart_read(void *buf, size_t count) {
-  mutex_lock(&uart_lock);
+//  mutex_lock(&uart_lock);
   mini_uart_getn(/* verbose */ false, buf, count);
-  mutex_unlock(&uart_lock);
+//  mutex_unlock(&uart_lock);
   /* For now, just assume all "count" bytes can be read. */
   return count;
 }
 
 size_t do_uart_write(const void *buf, size_t count) {
 //  mutex_lock(&uart_lock);
-//  mini_uart_putn(buf, count);
-    mini_uart_puts(buf);
+  mini_uart_putn(buf, count);
 //  mutex_unlock(&uart_lock);
   /* For now, just assume all "count" bytes can be written. */
   return count;
