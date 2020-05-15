93fbf3fc09e5d58f6ba8011a5e8b812b5aa047bd
JackGrence <green1181772@gmail.com>
2020-05-15 22:49:04 +0800

    [user] Add basic cli

--- user/program/shell.c
+++ user/program/shell.c
@@ -1,8 +1,35 @@
 #include <stdlib.h>
+#define BUF_SIZE 0x30
+
+void
+read_command (char *buf, size_t len)
+{
+  char c;
+  size_t i;
+  for (i = 0; i < len - 1; ++i)
+    {
+      uart_read (&c, 1);
+      uart_write (&c, 1);
+      if (c == '\r')
+	{
+	  c = '\n';
+	  uart_write (&c, 1);
+	  break;
+	}
+      buf[i] = c;
+    }
+  buf[i] = '\0';
+}
 
 int
 main ()
 {
-  uart_write ("asdf", 4);
+  char buf[BUF_SIZE];
+  while (1)
+    {
+      uart_write ("# ", 2);
+      read_command (buf, BUF_SIZE);
+      printf ("%s: command not found\r\n", buf);
+    }
   return 0;
 }
