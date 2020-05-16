61d469c134c9937bc96ee33ac49e3c8174eae303
JackGrence <green1181772@gmail.com>
2020-05-16 23:36:48 +0800

    [user] Add mmap.h

---
+++ user/lib/mmap.h
@@ -0,0 +1,13 @@
+#ifndef MMAP
+#define MMAP
+
+#include <stddef.h>
+#define MAP_FIXED 1
+#define MAP_ANONYMOUS 2
+#define MAP_POPULATE 4
+#define PROT_NONE 1
+#define PROT_READ 2
+#define PROT_WRITE 4
+#define PROT_EXEC 8
+
+#endif /* ifndef MMAP */
