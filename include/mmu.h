0ba8b0c73f7383e0177b633f70220d68b1293ae7
kaiiiz <at881005@gmail.com>
2020-05-17 23:36:06 +0800

    setup mair_el1

---
+++ include/mmu.h
@@ -0,0 +1,30 @@
+#ifndef __MMU_H__
+#define __MMU_H__
+
+// SCTLR_EL1, System Control Register (EL1)
+
+#define SCTLR_VALUE_MMU_DISABLED 0
+#define SCTLR_VALUE_MMU_ENABLE   1
+
+// TCR_EL1, Translation Control Register
+
+#define TTBR0_EL1_REGION_BIT    48
+#define TTBR1_EL1_REGION_BIT    48
+#define TTBR0_EL1_GRANULE       0b00 // 4KB
+#define TTBR1_EL1_GRANULE       0b10 // 4KB
+
+#define TCR_EL1_T0SZ            ((64 - TTBR0_EL1_REGION_BIT) << 0)
+#define TCR_EL1_T1SZ            ((64 - TTBR1_EL1_REGION_BIT) << 16)
+#define TCR_EL1_TG0             (TTBR0_EL1_GRANULE << 14)
+#define TCR_EL1_TG1             (TTBR1_EL1_GRANULE << 30)
+#define TCR_EL1_VALUE           (TCR_EL1_T0SZ | TCR_EL1_T1SZ | TCR_EL1_TG0 | TCR_EL1_TG1)
+
+// MAIR_EL1, Memory Attribute Indirection Register
+
+#define MAIR_DEVICE_nGnRnE      0b00000000
+#define MAIR_NORMAL_NOCACHE     0b01000100 // Normal memory, Outer Non-cacheable | Normal memory, Inner Non-cacheable
+#define MAIR_IDX_DEVICE_nGnRnE  0
+#define MAIR_IDX_NORMAL_NOCACHE 1
+#define MAIR_VALUE              ((MAIR_DEVICE_nGnRnE << (MAIR_IDX_DEVICE_nGnRnE * 8)) | (MAIR_NORMAL_NOCACHE << (MAIR_IDX_NORMAL_NOCACHE * 8)))
+
+#endif
--- include/sysregs.h
+++ include/sysregs.h
@@ -1,10 +1,6 @@
 #ifndef __SYSREGS_H__
 #define __SYSREGS_H__
 
-// SCTLR_EL1, System Control Register (EL1)
-
-#define SCTLR_VALUE_MMU_DISABLED 0
-
 // HCR_EL2, Hypervisor Configuration Register
 
 #define HCR_EL2_RW_AARCH64  (1 << 31)
@@ -26,19 +22,5 @@
 
 #define CPACR_EL1_FPEN      (0b11 << 20)
 #define CPACR_EL1_VALUE     (CPACR_EL1_FPEN)
-
-// TCR_EL1, Translation Control Register
-
-#define TTBR0_EL1_REGION_BIT    48
-#define TTBR1_EL1_REGION_BIT    48
-#define TTBR0_EL1_GRANULE       0b00 // 4KB
-#define TTBR1_EL1_GRANULE       0b10 // 4KB
-
-#define TCR_EL1_T0SZ            ((64 - TTBR0_EL1_REGION_BIT) << 0)
-#define TCR_EL1_T1SZ            ((64 - TTBR1_EL1_REGION_BIT) << 16)
-#define TCR_EL1_TG0             (TTBR0_EL1_GRANULE << 14)
-#define TCR_EL1_TG1             (TTBR1_EL1_GRANULE << 30)
-
-#define TCR_EL1_VALUE           (TCR_EL1_T0SZ | TCR_EL1_T1SZ | TCR_EL1_TG0 | TCR_EL1_TG1)
 
 #endif
