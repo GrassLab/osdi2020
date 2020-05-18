4125e8c86a8fb111b1422a6aa8f97f2ff0b0e5b4
cheng-yen tsai <evan11401@gmail.com>
2020-05-18 19:24:36 +0800

    req 1 ok

--- lab5/include/mmu.h
+++ lab5/include/mmu.h
@@ -1,16 +1,65 @@
+#ifndef _MMU_H
+#define _MMU_H
+
+#define VA_START 			0xffff000000000000
+#define PHYS_MEMORY_SIZE 		0x40000000	
+
+#define PAGE_SHIFT	 	12
+#define TABLE_SHIFT 		9
+#define SECTION_SHIFT		(PAGE_SHIFT + TABLE_SHIFT)
+
+#define PAGE_SIZE   		(1 << PAGE_SHIFT)  //2^12	
+#define SECTION_SIZE		(1 << SECTION_SHIFT) //2^21 
+
+#define HIGH_MEMORY             DEVICE_BASE // 0x3F00 0000
+#define LOW_MEMORY              (3 * SECTION_SIZE) // 3 * 2^21 = 6M
+                                                   // =0x60 0000 
+
+#define PTRS_PER_TABLE			(1 << TABLE_SHIFT)						   
+#define PGD_SHIFT			(PAGE_SHIFT + 3*TABLE_SHIFT) //39
+#define PUD_SHIFT			(PAGE_SHIFT + 2*TABLE_SHIFT) //30
+#define PMD_SHIFT			(PAGE_SHIFT + TABLE_SHIFT)   //21
+#define PTE_SHIFT                       (PAGE_SHIFT)                 //12
+
+#define PAGE_ENTRY            (HIGH_MEMORY/PAGE_SIZE)
+#define FIRST_AVAILIBLE_PAGE  (LOW_MEMORY/PAGE_SIZE)
+
+#define NOT_USED 0 
+#define USED_NOW 1
+#define PRESERVE 2
+
+
+#define SCTLR_RESERVED                  (3 << 28) | (3 << 22) | (1 << 20) | (1 << 11)
+#define SCTLR_EE_LITTLE_ENDIAN          (0 << 25)
+#define SCTLR_I_CACHE_DISABLED          (0 << 12)
+#define SCTLR_D_CACHE_DISABLED          (0 << 2)
+#define SCTLR_MMU_DISABLED              (0 << 0)
+#define SCTLR_VALUE_MMU_DISABLED	(SCTLR_RESERVED | SCTLR_EE_LITTLE_ENDIAN | SCTLR_I_CACHE_DISABLED | SCTLR_D_CACHE_DISABLED | SCTLR_MMU_DISABLED)
+
+/** addressible region is 48bit **/
 #define TCR_CONFIG_REGION_48bit (((64 - 48) << 0) | ((64 - 48) << 16))
+/** Page granule size is 4KB **/
 #define TCR_CONFIG_4KB ((0b00 << 14) |  (0b10 << 30))
 #define TCR_CONFIG_DEFAULT (TCR_CONFIG_REGION_48bit | TCR_CONFIG_4KB)
 
+/**  Device-nGnRnE memory **/
 #define MAIR_DEVICE_nGnRnE 0b00000000
+/** Normal memory, Outer Non-cacheable **/
+/** Normal memory, Inner Non-cacheable **/
 #define MAIR_NORMAL_NOCACHE 0b01000100
 #define MAIR_IDX_DEVICE_nGnRnE 0
 #define MAIR_IDX_NORMAL_NOCACHE 1
 #define MAIR_CONFIG_DEFAULT  (MAIR_DEVICE_nGnRnE << (MAIR_IDX_DEVICE_nGnRnE * 8)) | (MAIR_NORMAL_NOCACHE << (MAIR_IDX_NORMAL_NOCACHE * 8)) 
 
-
 #define PD_TABLE 0b11
+#define PD_PAGE  0b11
 #define PD_BLOCK 0b01
 #define PD_ACCESS (1 << 10)
-#define BOOT_PGD_ATTR PD_TABLE
-#define BOOT_PUD_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
+#define PD_ACCESS_PERMISSION	(1 << 6) 
+
+#define MMU_FLAGS (PD_ACCESS | (MAIR_IDX_NORMAL_NOCACHE << 2) | PD_PAGE)
+#define MMU_DEVICE_FLAGS   (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_PAGE)
+#define MMU_PTE_FLAGS   (PD_ACCESS | (MAIR_IDX_NORMAL_NOCACHE << 2) | PD_PAGE | PD_ACCESS_PERMISSION)
+
+
+#endif
