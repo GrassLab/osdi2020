b2806af19865515af56c76fb50a9209fa519950a
guo <qaz813887@gmail.com>
2020-05-18 13:34:10 -0700

    require 1-5 (3 level with pag table in tail)

--- lab5/main.c
+++ lab5/main.c
@@ -89,8 +89,8 @@ void kernel_init()
 	unsigned long long int sp;
 	uart_init();
 	asm volatile("mov %0, sp":"=r"(sp)::);
-aaaaa:
-	uart_hex((unsigned long long)&&aaaaa);
+//aaaaa:
+	//uart_hex((unsigned long long)&&aaaaa);
 	//my_printf("hahaha\r\n");
 	uart_puts("hahaha\r\n");
 	while(1);
