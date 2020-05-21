0a8abdd645e7b15745bec4d6aff2813c98202d40
john0829 <joh860829nny@gmail.com>
2020-05-21 23:07:17 +0800

    temp

--- lab5/include/mm.h
+++ lab5/include/mm.h
@@ -22,13 +22,23 @@
 
 #define PD_TABLE 0b11
 #define PD_BLOCK 0b01
+#define PD_PAGE  0b11
 #define PD_ACCESS (1 << 10)
+#define PD_ACCESS_PERMISSION	(1 << 6)
+#define PD_READONLY     (1 << 7)
+#define PD_NON_EXEC_EL0 (1UL << 54)
+
 #define BOOT_PGD_ATTR PD_TABLE
 #define BOOT_PUD_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
+
+#define PGD_ATTR PD_TABLE
+#define PUD_ATTR PD_TABLE
+#define PMD_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
 
 #ifndef __ASSEMBLER__
 
 void memzero(unsigned long src, unsigned long n);
+void virt_mem_init();
 
 #endif
 
