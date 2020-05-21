7a7cc0148e2634fb4a510d6b678fbbc3268cbbc0
john0829 <joh860829nny@gmail.com>
2020-05-21 20:02:37 +0800

    temp

--- lab5/include/mm.h
+++ lab5/include/mm.h
@@ -11,6 +11,21 @@
 #define EL1_LOW_MEMORY              	(2 * SECTION_SIZE)
 #define EL0_LOW_MEMORY              (4 * SECTION_SIZE)
 
+#define TCR_CONFIG_REGION_48bit (((64 - 48) << 0) | ((64 - 48) << 16))
+#define TCR_CONFIG_4KB ((0b00 << 14) |  (0b10 << 30))
+#define TCR_CONFIG_DEFAULT (TCR_CONFIG_REGION_48bit | TCR_CONFIG_4KB)
+
+#define MAIR_DEVICE_nGnRnE 0b00000000
+#define MAIR_NORMAL_NOCACHE 0b01000100
+#define MAIR_IDX_DEVICE_nGnRnE 0
+#define MAIR_IDX_NORMAL_NOCACHE 1
+
+#define PD_TABLE 0b11
+#define PD_BLOCK 0b01
+#define PD_ACCESS (1 << 10)
+#define BOOT_PGD_ATTR PD_TABLE
+#define BOOT_PUD_ATTR (PD_ACCESS | (MAIR_IDX_DEVICE_nGnRnE << 2) | PD_BLOCK)
+
 #ifndef __ASSEMBLER__
 
 void memzero(unsigned long src, unsigned long n);
