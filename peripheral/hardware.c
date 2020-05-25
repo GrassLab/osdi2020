79b46f3fd478ecc23400169d8dbf51b0138f3ead
JackGrence <green1181772@gmail.com>
2020-05-25 10:46:40 +0800

    Add function to get memeory size

--- peripheral/hardware.c
+++ peripheral/hardware.c
@@ -40,3 +40,26 @@ hardware_info_VC_base ()
 
   return *(unsigned int *) (msg + 1);
 }
+
+size_t
+hardware_info_memory_size ()
+{
+  unsigned int *base_addr;
+  unsigned int *size;
+  struct mbox_msg *msg;
+  msg = (struct mbox_msg *) &mbox;
+  // set tag
+  msg->tag.id = 0x00010005;
+  msg->tag.buf_size = 8;
+  msg->tag.code = 0;
+  // set mseeage
+  msg->buf_size = sizeof (struct mbox_msg) + msg->tag.buf_size + 4;
+  msg->code = 0;
+  // set tag buf and tag end
+  bzero (msg + 1, msg->tag.buf_size + 4);
+  mbox_call (MBOX_CH_PROP);
+
+  base_addr = (unsigned int *) (msg + 1);
+  size = base_addr + 1;
+  return *size;
+}
--- peripheral/hardware.h
+++ peripheral/hardware.h
@@ -1,6 +1,8 @@
 #ifndef HARDWARE
 #define HARDWARE
 
+#include <stddef.h>
 unsigned int hardware_info_VC_base ();
 unsigned int hardware_info_revision ();
+size_t hardware_info_memory_size ();
 #endif /* ifndef HARDWARE */
