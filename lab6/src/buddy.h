d53300ac726524e015349d8d75aa940083180d1a
chihcenyang <chichensmile@gmail.com>
2020-06-02 13:58:24 +0800

    finish req1 buddy system

---
+++ lab6/src/buddy.h
@@ -0,0 +1,42 @@
+#ifndef _BUDDY_H
+#define _BUDDY_H
+
+#define MAX_BUDDY_PAGE_NUM	(9)
+#define PAGE_NUM_FOR_MAX_BUDDY	((1<<MAX_BUDDY_PAGE_NUM)-1)
+
+/*page flags*/
+#define PAGE_AVAILABLE		0x00
+#define PAGE_DIRTY			0x01
+#define PAGE_PROTECT		0x02
+#define PAGE_BUDDY_BUSY		0x04
+#define PAGE_IN_CACHE		0x08
+
+/*memory for dynamic allocation 8MB from 0x00010000 ~ 0x00810000*/
+#define _MEM_END	HIGH_MEMORY //modify
+#define _MEM_START	LOW_MEMORY //modify
+
+/*MASK is 0xfffff000*/
+#define PAGE_MASK	(~(PAGE_SIZE-1))
+
+#define KERNEL_MEM_END	(_MEM_END)
+/*the begin and end of the kernel mem which is needed to be paged. 4K alignment*/
+#define KERNEL_PAGING_START	((_MEM_START+(~PAGE_MASK))&((PAGE_MASK)))
+/* The number of dynamic memory space divided struct page needed*/
+#define	KERNEL_PAGING_END	(((KERNEL_MEM_END-KERNEL_PAGING_START)/(PAGE_SIZE+sizeof(struct page)))*(PAGE_SIZE)+KERNEL_PAGING_START)
+
+/*the number of pages*/
+#define KERNEL_PAGE_NUM	((KERNEL_PAGING_END-KERNEL_PAGING_START)/PAGE_SIZE)
+
+/*the memory space uses for store struct page from KERNEL_PAGE_START to _MEM_END*/
+#define KERNEL_PAGE_END	_MEM_END
+#define KERNEL_PAGE_START	(KERNEL_PAGE_END-KERNEL_PAGE_NUM*sizeof(struct page))
+
+/*we can do these all because the page structure that represents one page aera is continuous*/
+#define BUDDY_END(x,order)	((x)+(1<<(order))-1)
+#define NEXT_BUDDY_START(x,order)	((x)+(1<<(order)))
+#define PREV_BUDDY_START(x,order)	((x)-(1<<(order)))
+
+unsigned long get_free_page(int order);
+void free_page(unsigned long p);
+void init_page_map(void);
+#endif
---
+++ lab6/src/mm.c
@@ -0,0 +1,27 @@
+#include "mm.h"
+
+static unsigned short mem_map [ PAGING_PAGES ] = {0,};
+/*
+unsigned long get_free_page()
+{
+    for (int i = 0; i < PAGING_PAGES; i++){
+        if (mem_map[i] == 0){
+            mem_map[i] = 1;
+            return LOW_MEMORY + i*PAGE_SIZE;
+        }
+    }
+    return 0;
+}
+
+void free_page(unsigned long p){
+    mem_map[(p - LOW_MEMORY) / PAGE_SIZE] = 0;
+}
+*/
+void fork_memcpy (void *dest, const void *src, unsigned long len)
+{
+    char *d = dest;
+    const char *s = src;
+    while (len--)
+            *d++ = *s++;
+}
+
