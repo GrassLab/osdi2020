66024b5e7bf67a2ee2f3f69628e8900147ed2769
hchsu(Arch) <hchsu0426@cs.nctu.edu.tw>
2020-06-12 16:20:23 +0800

    Revise Test Case

--- lab7/test.c
+++ lab7/test.c
@@ -7,25 +7,42 @@ int main(void)
   char buf[0x20];
   syscall_uart_puts("Hi I'm test in user mode\n");
 
-  int a = syscall_open("/hello", O_CREAT);
+  /* ls */
+  int root = syscall_open("/", 0);
+  syscall_list(root);
+  syscall_close(root);
 
+  int a = syscall_open("/hello", O_CREAT);
   int b = syscall_open("/world", O_CREAT);
   syscall_write(a, "Hello ", 6);
   syscall_write(b, "World!", 6);
   syscall_close(a);
   syscall_close(b);
-  b = syscall_open("/hello", 0);
-  a = syscall_open("/world", 0);
-  int sz;
-  sz = syscall_read(b, buf, 100);
-  sz += syscall_read(a, buf + sz, 100);
-  buf[sz] = '\0';
-  syscall_uart_puts(buf);
-  syscall_uart_puts("\n");
 
-  /* ls */
-  int root = syscall_open("/", 0);
-  syscall_list(root);
-  syscall_close(root);
+  if(syscall_fork() == 0)
+  {
+    syscall_uart_puts("Hi I'm child\n");
+    a = syscall_open("/world", 0);
+    b = syscall_open("/hello", 0);
+    int sz;
+    sz = syscall_read(a, buf, 100);
+    sz += syscall_read(b, buf + sz, 100);
+    buf[sz] = '\0';
+    syscall_uart_puts(buf);
+    syscall_uart_puts("\n");
+  }
+  else
+  {
+    syscall_uart_puts("Hi I'm parent\n");
+    b = syscall_open("/hello", 0);
+    a = syscall_open("/world", 0);
+    int sz;
+    sz = syscall_read(b, buf, 100);
+    sz += syscall_read(a, buf + sz, 100);
+    buf[sz] = '\0';
+    syscall_uart_puts(buf);
+    syscall_uart_puts("\n");
+  }
+
 }
 
